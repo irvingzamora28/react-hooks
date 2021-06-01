5e6db2b69284c35031d22f764a0dcc036e5b84d4
Irving Zamora <irvingzamora28@gmail.com>
2021-05-31 19:46:14 -0600

    Added useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
-	// const [count, setCount] = useState(() => {
-	// 	return 0
-	// })
+	
+	console.log("Rendered Start");
 
 	const [state, setState] = useState({count: 4, theme: 'blue'})
 	const count = state.count
 	const theme = state.theme
+
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+	const handleResize = () => {
+		setWindowWidth(window.innerWidth)
+	}
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize)
+		return () => {
+			// Cleanup event listener
+			window.removeEventListener('resize', handleResize)
+		}
+	}, [])
+
+	const [items, setItems] = useState([])
+	const [resourceType, setResourceType] = useState('posts')
+
+	useEffect(() => {
+		console.log("Effect");
+		fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+		.then(response => response.json())
+		.then(json => setItems(json))
+		return () => {
+			console.log(`Cleanup ${resourceType}`);
+		}
+	}, [resourceType])
 
 	function decreementCount() {
 		// setCount((prevCount) => prevCount - 1)
@@ -22,10 +48,20 @@ function App() {
 	}
 	return (
 		<div>
+			<h1>WIndows width</h1>
+			<h2 >{windowWidth}</h2 >
 			<button onClick={decreementCount}>-</button>
 			<span>{count}</span>
 			<span>{theme}</span>
 			<button onClick={incrementCount}>+</button>
+
+			<div>
+				<button onClick={() => setResourceType('posts')}>Posts</button>
+				<button onClick={() => setResourceType('users')}>Users</button>
+				<button onClick={() => setResourceType('comments')}>Comments</button>
+			</div>
+			<h1>{resourceType}</h1>
+			{items.map(item => { return <pre>{JSON.stringify(item)}</pre>})}
 		</div>
 	);
 }
